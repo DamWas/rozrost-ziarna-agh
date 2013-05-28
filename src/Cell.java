95f6a5af808a88ad3d0404945baf7aa7754f7a70
mkaflowski <mkaflowski@d578a3a8-a345-81a6-b6f2-e1c5a6333108>
2013-05-28 19:33:44 +0000

    Dodano funkcje generującą kolejny cykl życia

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,9 @@
-
+/**Klasa reprezentuj¹ca ziarno/komórkê
+	 * @author Mateusz Kaflowski, Marcin G³adosz, Krystian Bersztolc, Witold
+	 *         Gramatyka, Micha³ Grabarczyk
+	 * @version 1.0
+	 * @since 2013-05-24
+	 */
 public class Cell {
 
 	public Cell(int i, int j) {
