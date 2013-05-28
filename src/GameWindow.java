bed962a9607a48542f4ed83a22aee5c8a0cf07aa
vitek990 <vitek990@gmail.com>
2013-05-28 17:05:42 +0000

    Stworzenie podstaw okna wizualizacji przebiegu rozrostu

---
+++ src/GameWindow.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.ArrayList;
+ 
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+ 
+/**Klasa wizualizuj¹ca kolejne cykle ¿ycia.
+ * @author Mateusz Kaflowski, Marcin G³adosz, Krystian Bersztolc, Witold
+ *         Gramatyka, Micha³ Grabarczyk
+ * @version 1.0
+ * @since 2013-05-27
+ */
+public class GameWindow extends JPanel {
+ 
+        /**Automat na którym dzia³amy*/
+        static Automat automat;
+       
+        /**Szerokoœæ pojedynczej komórki.*/
+        public static final int WIDTH = 5;
+        /**OpóŸnienie miêdzy cyklami ¿ycia.*/
+        public static int delay = 100;
+ 
+        public static int thickness = 1;
+        /**Kolekcja wszystkich kolorów reprezentuj¹cych rodzaje ziaren.*/
+        public static ArrayList<Color> colors;
+        /**Liczba wszytkich kolorów reprezentuj¹cych rodzaje ziaren.*/
+        public static int colorCounter = 0;
+ 
+        public GameWindow() {
+ 
+                setBorder(BorderFactory.createLineBorder(Color.PINK));
+ 
+                automat = new Automat(100);
+                colors = new ArrayList<Color>();
+        }
+ 
+        public Dimension getPreferredSize() {
+                return new Dimension(500, 500);
+        }
+ 
+ 
+}
