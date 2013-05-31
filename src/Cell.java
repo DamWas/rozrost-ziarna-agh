2dd9a3fa88abf0c88de9d049058340e2eb1a20f0
vitek990 <vitek990@gmail.com>
2013-05-31 22:31:14 +0000

    Funkcja dodająca dyslokację do komórki oraz sprawdzajaca czy ziarno lezy na granicy

--- src/Cell.java
+++ src/Cell.java
@@ -10,4 +10,36 @@ public class Cell {
 		// TODO Auto-generated constructor stub
 	}
 
+   /**Funkcja dodaj¹ca dyslokacjê do komórki
+         * @param pDislocation dyslokacja do dodania
+         */
+        public void addDislocation(double pDislocation) {
+                Random rand = new Random();
+                double toAdd = 0;
+                if (isOnBorder())
+                        toAdd = pDislocation * (rand.nextInt(110) + 70)/100;
+                else
+                        toAdd = pDislocation * rand.nextInt(30)/100;
+                disloacation += toAdd;
+                Recrystallization.dislocation -= toAdd;
+        }
+ 
+        /**Funkcja sparwdzaj¹ca czy ziarno le¿y na granicy*/
+        public boolean isOnBorder() {
+                for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++) {
+                                if (x + i < 0 || x + i >= GameWindow.automat.size || y + j < 0
+                                                || y + j >= GameWindow.automat.size)
+                                        continue;
+                                if (GameWindow.automat.tab[x][y] != GameWindow.automat.tab[x + i][y + j])
+                                        return true;
+ 
+                        }
+                return false;
+        }
+
+
+
+
+
 }
